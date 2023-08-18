c75227aad2579bfd69233ab81b2bc3171fe44306
SeungGun <saverrw2864@naver.com>
2023-08-18 18:09:42 +0900

    feat: 태그 전체 조회 api 구현 (#5)

---
+++ src/main/java/com/namecard/tag/dto/result/TagResult.java
@@ -0,0 +1,20 @@
+package com.namecard.tag.dto.result;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class TagResult {
+
+    @Schema(description = "태그 PK")
+    private Long tagId;
+    @Schema(description = "태그 이름")
+    private String tagName;
+
+    @Builder
+    public TagResult(Long tagId, String tagName) {
+        this.tagId = tagId;
+        this.tagName = tagName;
+    }
+}
