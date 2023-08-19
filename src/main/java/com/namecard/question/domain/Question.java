7012139b2eeba1f2310821c12688eb76ad6f1a3d
SeungGun <saverrw2864@naver.com>
2023-08-19 09:51:56 +0900

    fix: 질문 엔티티의 유저와의 cascade 제거

--- src/main/java/com/namecard/question/domain/Question.java
+++ src/main/java/com/namecard/question/domain/Question.java
@@ -19,7 +19,7 @@ public class Question extends AuditBaseEntity {
     @Schema(description = "사용자 설정 질문 PK")
     private Long questionId;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId")
     @Schema(description = "사용자 설정 질문을 만들 유저")
     private Users users;
