99a801ebfa617e381cda2f342a719d1cd4b8ad4c
SeungGun <saverrw2864@naver.com>
2023-09-08 16:25:33 +0900

    refactor: 토큰 발급 api path 수정

--- src/main/java/com/namecard/auth/service/AuthService.java
+++ src/main/java/com/namecard/auth/service/AuthService.java
@@ -3,10 +3,10 @@ package com.namecard.auth.service;
 import com.namecard.auth.dto.request.SendAuthRequest;
 import com.namecard.auth.dto.request.ValidAuthRequest;
 import com.namecard.auth.dto.result.AuthResult;
-import com.namecard.config.JwtConfig;
+import com.namecard.config.jwt.JwtConfig;
 import com.namecard.exception.UnauthorizedException;
-import com.namecard.users.dto.entity.Users;
-import com.namecard.users.dto.result.LoginResult;
+import com.namecard.member.domain.Member;
+import com.namecard.member.dto.result.LoginResult;
 import com.namecard.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,7 +25,7 @@ public class AuthService {
     private final RedisService redisService;
 
     @Transactional(rollbackFor = Exception.class)
-    public LoginResult newAccessToken(HttpServletRequest request) {
+    public LoginResult generateNewToken(HttpServletRequest request) {
         String accessToken = jwtConfig.extractAccessToken(request).orElseThrow(
                 () -> new UnauthorizedException("엑세스 토큰이 필요합니다.")
         );
@@ -33,18 +33,21 @@ public class AuthService {
                 () -> new UnauthorizedException("리프레시 토큰이 없습니다. 로그인이 필요합니다.")
         );
 
-        Optional<String> SMemberNo = jwtConfig.extractMemberNo(accessToken);
+        // JWT 엑세스 토큰으로부터 회원 ID 추출
+        Optional<String> strMemberId = jwtConfig.extractMemberNo(accessToken);
         long memberNo = 0;
-        if(SMemberNo.isPresent()) {
-            memberNo = Long.parseLong(SMemberNo.get());
+        if (strMemberId.isPresent()) {
+            memberNo = Long.parseLong(strMemberId.get());
         }
 
         String redisRefreshToken = redisService.findRefreshToken(memberNo);
 
-        if(refreshToken.equals(redisRefreshToken) && jwtConfig.isTokenValid(refreshToken)) {
-            Users users = Users.builder().userId(memberNo).build();
-            LoginResult result = redisService.tokenCreate(users);
-            return result;
+        // 요청으로 받은 refresh 토큰과 redis에 저장된 토큰 비교하고 요청으로 받은 토큰이 유효하면
+        if (refreshToken.equals(redisRefreshToken) && jwtConfig.isTokenValid(refreshToken)) {
+            Member member = Member.builder()
+                    .memberId(memberNo)
+                    .build();
+            return redisService.createNewTokens(member);
         }
         throw new UnauthorizedException("로그인이 필요합니다.");
     }
@@ -54,7 +57,9 @@ public class AuthService {
         String authCode = this.randomAuthCode();
         redisService.savePhoneAuthCode(request.getPhoneNum(), authCode);
 
-        return AuthResult.builder().authCode(authCode).build();
+        return AuthResult.builder()
+                .authCode(authCode)
+                .build();
     }
 
     private String randomAuthCode() {
@@ -70,10 +75,10 @@ public class AuthService {
     @Transactional(rollbackFor = Exception.class)
     public boolean validAuth(ValidAuthRequest request) {
         String redisAuthCode = redisService.findPhoneAuthCode(request.getPhoneNum());
-        if(redisAuthCode == null) {
+        if (redisAuthCode == null) {
             throw new IllegalArgumentException("인증 정보가 없습니다.");
         }
-        if(redisAuthCode.equals(request.getAuthCode())) {
+        if (redisAuthCode.equals(request.getAuthCode())) {
             redisService.delete(request.getPhoneNum());
             redisService.savePhoneAuthSuccess(request.getPhoneNum());
             return true;
