5b6c7e9483d96714225f98830425c4650555a62b
SeungGun <saverrw2864@naver.com>
2023-09-10 01:35:14 +0900

    refactor: 내 프로필 요청/응답 dto에 공개 여부 필드 추가

--- src/main/java/com/namecard/member/dto/request/MyProfileRequest.java
+++ src/main/java/com/namecard/member/dto/request/MyProfileRequest.java
@@ -1,5 +1,6 @@
 package com.namecard.member.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -7,4 +8,10 @@ public class MyProfileRequest {
     private String phoneNum;
     private String email;
     private String introduce;
+    @JsonProperty("isPublicEmail")
+    private boolean isPublicEmail;
+    @JsonProperty("isPublicPhone")
+    private boolean isPublicPhone;
+    @JsonProperty("isPublicIntroduce")
+    private boolean isPublicIntroduce;
 }
--- src/main/java/com/namecard/member/dto/result/MyProfileResult.java
+++ src/main/java/com/namecard/member/dto/result/MyProfileResult.java
@@ -1,5 +1,6 @@
 package com.namecard.member.dto.result;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
@@ -17,13 +18,22 @@ public class MyProfileResult {
     @Schema(description = "사용자 이메일")
     private String email;
 
-    @Schema(description = "사용자 닉네임")
-    private String nickname;
-
     @Schema(description = "사용자 전화번호")
     private String phoneNum;
 
     @Schema(description = "자기소개")
     private String introduce;
+
+    @Schema(description = "이메일 공개 여부")
+    @JsonProperty("isPublicEmail")
+    private boolean isPublicEmail;
+
+    @Schema(description = "전화번호 공개 여부")
+    @JsonProperty("isPublicPhone")
+    private boolean isPublicPhone;
+
+    @Schema(description = "자기소개 공개 여부")
+    @JsonProperty("isPublicIntroduce")
+    private boolean isPublicIntroduce;
 
 }
